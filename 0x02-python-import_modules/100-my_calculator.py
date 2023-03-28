656c2cfae9fb2c7fc9a73c44f6528c7e9ff4dfdf
kenkomu <komukenneth216@gmail.com>
2023-03-28 21:48:54 +0300

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+
 if __name__ == "__main__":
-    """
-    Handle basic arithmetic operations.
-    """
-    from calculator_1 import add, sub, mul, div
-    import sys
-    if len(sys.argv) -1 !=3:
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
+
     a = int(sys.argv[1])
-    b = int(sys.argv[3])
     operator = sys.argv[2]
+    b = int(sys.argv[3])
+
     if operator == '+':
         result = add(a, b)
     elif operator == '-':
@@ -22,4 +22,5 @@ if __name__ == "__main__":
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
+
+    print("{} {} {} = {}".format(a, operator, b, result))
