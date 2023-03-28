8e95e74d01ddff5fbb01acbe38b6dc5e53f55cbc
kenkomu <komukenneth216@gmail.com>
2023-03-28 21:46:20 +0300

    fixing pycodestyle errors

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    sum = 0
-    for arg in sys.argv[1:]:
-        sum += int(argv)
-        print(f"sum")
+    args = sys.argv[1:]
+    total = sum(int(arg) for arg in args)
+    print(total)
