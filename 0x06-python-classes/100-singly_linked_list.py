1d8f42e9c5dc4f669b525fbb2fa95ff9b8069a59
kenkomu <komukenneth216@gmail.com>
2023-04-21 01:55:39 +0300

    class node

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -23,34 +23,3 @@ class Node:
         if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
         self._next_node = value
-
-
-class SinglyLinkedList:
-    def __init__(self):
-        self.head = None
-    
-    def __str__(self):
-        nodes = []
-        current = self.head
-        while current:
-            nodes.append(str(current.data))
-            current = current.next_node
-        return "\n".join(nodes)
-    
-    def sorted_insert(self, value):
-        new_node = Node(value)
-        if not self.head:
-            self.head = new_node
-            return
-        
-        current = self.head
-        prev = None
-        while current and current.data < value:
-            prev = current
-            current = current.next_node
-        if not prev:
-            new_node.next_node = self.head
-            self.head = new_node
-        else:
-            prev.next_node = new_node
-            new_node.next_node = current
