8213fcf82b7ce81f9b9e186e128add9173c2231f
jordanj0731 <jordanj0731@gmail.com>
2023-05-29 12:31:52 +0300

    8-count_89.sql

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,2 @@
---script that displays the number of records with id = 89 in the table first_table
-
+-- script that displays the number of records with id = 89 in the table first_table
 SELECT COUNT(id) FROM first_table WHERE (id = 89);
