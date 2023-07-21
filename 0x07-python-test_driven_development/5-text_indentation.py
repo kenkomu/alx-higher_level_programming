b36b859a2b02239f5509b83027f36e4b99d3dcc6
kenkomu <komukenneth216@gmail.com>
2023-07-21 07:32:24 +0300

    5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,31 +1,37 @@
 #!/usr/bin/python3
+"""This module consists of a single function that prints a texts with
+new lines when a special character is encountered.
 
-"""text_indentation module"""
+In this module a function called text_indentation() is specified and its
+functionality consists of printing a text and adding two new lines each time
+a ? . or : is found.
+
+Example:
+    An example in which the function is implemented is the following.
+
+    text_indentation("Hello. World")
+    Hello
+
+    World
+
+"""
 
 
 def text_indentation(text):
-    """text_indentation function
-
-    prints a text with 2 new lines
-    Attributes:
-        text = the text
-    Raises:
-        TypeError: If text is not a string.
+    """ prints a text with 2 new lines after the characters: ., ? and :
+    Args:
+        text (str): text to divide.
     """
-    if isinstance(text, str):
+    if type(text) != str:
         raise TypeError("text must be a string")
-
-    prmText = prmText.strip()
-    prmText = prmText.replace(".", ".\n")
-    prmText = prmText.replace("?", "?\n")
-    prmText = prmText.replace(":", ":\n")
-    prmText = prmText.rstrip()
-
-    if len(prmText) == 0:
-        return
-
-    for key, line in enumerate(list(prmText.split("\n"))):
-        line = line.strip()
-        print("{}".format(line))
-        if key < len(list(prmText.split("\n"))) - 1:
-            print()
+    i = 0
+    while i < len(text):
+        if text[i] == "." or text[i] == "?" or text[i] == ":":
+            print("{}\n\n".format(text[i]), end="")
+            i += 1
+            while text[i] == " ":
+                i += 1
+                continue
+            continue
+        print(text[i], end="")
+        i += 1
