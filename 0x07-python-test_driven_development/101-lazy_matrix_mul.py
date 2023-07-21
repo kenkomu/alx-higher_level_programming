626ffd0efaba37721fe7a51637dfc9221b18f566
kenkomu <komukenneth216@gmail.com>
2023-07-21 07:54:37 +0300

    101-lazy_matrix_mul.py

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,34 +1,18 @@
 #!/usr/bin/python3
-""" lazy_matrix_mul module """
+"""
+lazy_matrix module
+This function calculates multiplication of matrix using numpy lib
 
+"""
 
 import numpy as np
 
 
-def lazy_matrix_mul(prmMatrixA, prmMatrixB):
-    """ lazy_matrix_mul function
-    this function multiply one matrix by a second one
-    Attributes:
-        prmMatrixA: first matrix
-        prmMatrixB: second matrix
+def lazy_matrix_mul(m_a, m_b):
+    """Return: a nex matrix with the product of m_a and m_b
+    Args:
+    param1: m_a type list of list is a matrix
+    param2: m_b type list of list is a matrix
+    Raise: TypeError, ValueError
     """
-    if prmMatrixA is None:
-        raise TypeError("m_a should be indicate")
-    if prmMatrixB is None:
-        raise TypeError("m_b should be indicate")
-    if not isinstance(prmMatrixA, list):
-        raise TypeError("m_a must be a list")
-    if not isinstance(prmMatrixB, list):
-        raise TypeError("m_b must be a list")
-    if not all(isinstance(ele, list) for ele in prmMatrixA):
-        raise TypeError("m_a must be a list of lists")
-    if not all(isinstance(ele, list) for ele in prmMatrixB):
-        raise TypeError("m_b must be a list of lists")
-    if len(prmMatrixA) == 0 or len(prmMatrixA[0]) == 0:
-        raise TypeError("m_a can't be empty")
-    if len(prmMatrixB) == 0 or len(prmMatrixB[0]) == 0:
-        raise TypeError("m_b can't be empty")
-
-    arr1 = np.array(prmMatrixA)
-    arr2 = np.array(prmMatrixB)
-    return np.matmul(arr1, arr2)
+    return (np.matmul(m_a, m_b))
