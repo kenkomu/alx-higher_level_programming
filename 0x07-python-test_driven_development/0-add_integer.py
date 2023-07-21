4d29b6f0bcdd25a5a56f40356b23da46bf7a0913
kenkomu <komukenneth216@gmail.com>
2023-07-21 07:19:38 +0300

    initial commit

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,22 +1,33 @@
 #!/usr/bin/python3
-"""add_integer module"""
+"""This module consists of a single function that adds.
+
+In this module a function called add_integers is specified and its
+functionality consists of adding two values passed by parameter.
+
+Example:
+    An example in which the function needs to be implemented is the following.
+
+    add_integers(3, 4)
+    7
+
+    add_integers(3, -3)
+    0
+
+"""
 
 
 def add_integer(a, b=98):
-    """ add_integer function
-
-    this function add two integer
-
-    Attributes:
-        a: first integer
-        b: optional second integer (init by 98 by default)
+    """ Returns the addition of two parameters.
+    Args:
+        a (int): integer that represents the first number to add.
+        b (int): integer that represents the second number to add.
     """
-    if isinstance(a, float):
-        a = int(a)
-    if isinstance(b, float):
-        b = int(b)
-    if not isinstance(a, int):
+    if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
-    if not isinstance(b, int):
+    elif type(b) != int and type(b) != float:
         raise TypeError("b must be an integer")
-    return a + b
+    try:
+        sum = int(a) + int(b)
+        return sum
+    except OverflowError:
+        raise OverflowError("cannot convert float infinity to integer")
