af3d92c015b940316c1712f7bca93d82ef28a92f
kenkomu <komukenneth216@gmail.com>
2023-07-21 07:29:32 +0300

    3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
+"""This module consists of a single function that prints an introduction.
 
-"""say_my_name module"""
+In this module a function called say_my_name is specified and its
+functionality consists of saying an specified name and lastname passed
+by parameters.
+
+Example:
+    An example in which the function is implemented is the following.
+
+    say_my_name("Walter", "White")
+    Walter White
+
+
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    """say_my_name function
-
-    prints My name
-
-    args:
-        first_name = the first name
-        last_name = the last name
+    """ Prints a the name in a sentence.
+    Args:
+        first_name (str): first name to print.
+        last_name (str): last name to print.
     """
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
 
-    if not isinstance(first_name, str):
-        raise TypeError(
-            "first_name must be a string")
-    if not isinstance(last_name, str):
-        raise TypeError(
-            "last_name must be a string")
-    print("My name is {:d} {:d}".format(first_name, last_name))
+    print("My name is {} {}".format(first_name, last_name))
