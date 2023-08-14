11f1f73de9c9a92f2e14046a8339749a31cd4f3d
kenkomu <komukenneth216@gmail.com>
2023-08-14 09:41:25 +0300

    initial commit

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
-const { dict } = require('101-data');
-const newDict = {};
-for (const [, value] of Object.entries(dict)) {
-  const list = [];
-
-  for (const [key, dictValue] of Object.entries(dict)) {
-    if (dictValue === value) {
-      list.push(key);
-    }
+const dict = require('./101-data.js').dict;
+let newDict = {};
+for (let key in dict) {
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [key];
+  } else {
+    newDict[dict[key]].push(key);
   }
-
-  newDict[value] = list;
 }
-
 console.log(newDict);
