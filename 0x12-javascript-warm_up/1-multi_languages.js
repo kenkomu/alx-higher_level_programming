d637c28638a2df13782c8c66f0205caae48c6e44
kenkomu <komukenneth216@gmail.com>
2023-08-14 09:21:35 +0300

    initail commits

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,2 @@
 #!/usr/bin/node
-const str1 = ('C is fun');
-const str2 = ('Python is cool');
-const str3 = ('JavaScript is amazing')
-console.log(str1 + '\n' + str2 + '\n' + str3);
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
