2001de70e804a6811a3047978d0a0d2ce8707120
kenkomu <komukenneth216@gmail.com>
2023-08-14 10:55:57 +0300

    initial commit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""This module contains a script that lists all states from hbtn_0e_0_usa"""
+"""Lists all states"""
 
-import sys
 import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                           user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
